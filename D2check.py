1484dcef8c76cc0e29364436dfd8a32a90567a1f
D2Check <misterfyxit@gmail.com>
2019-09-14 13:05:45 -0400

    updated tool to only look for relevant file types

--- D2check.py
+++ D2check.py
@@ -2,11 +2,13 @@ from os import listdir
 from os.path import isfile, join
 import psutil
 import hashlib
+import sys
 
 # Get a process list
 psutil.process_iter(attrs=None, ad_value=None)
 
 # for each process in the process list
+found = 0
 for proc in psutil.process_iter():
 
     # Try to do the following things
@@ -14,9 +16,17 @@ for proc in psutil.process_iter():
         # Get process name & pid from process object.
         processName = proc.name()
         processID = proc.pid
-
+        print("Searching for d2 process")
         # If the process name is "Game.exe" we have found diablo 2 running
         if processName == "Game.exe":
+            print("D2 process found")
+            found += 1
+            if found >= 2:
+
+                # Protect against previously opening 2 or more d2 games
+                fout = open("log.txt", "w")
+                fout.close()
+                sys.exit()
             # Open a log file
             fout = open("log.txt", "w")
 
@@ -34,23 +44,25 @@ for proc in psutil.process_iter():
 
             # for each of the files in the directory
             for file in onlyfiles:
+                # we only want USEFUL files
+                if file.endswith("mpq") or file.endswith("dll") or file.endswith("exe"):
+                    # make a new sha256 hash object
+                    hasher = hashlib.sha256()
+                    with open(path + file, 'rb') as afile:
+                        buf = afile.read()
 
-                # make a new sha256 hash object
-                hasher = hashlib.sha256()
-                with open(path + file, 'rb') as afile:
-                    buf = afile.read()
+                        #update the hash object with the binary representation of the file
+                        hasher.update(buf)
+                    # prepare a line of content to exfil to the logfile
+                    line = "{}\t\t{}\n".format(hasher.hexdigest(),file[0:-4])
 
-                    #update the hash object with the binary representation of the file
-                    hasher.update(buf)
-                # prepare a line of content to exfil to the logfile
-                line = file + hasher.hexdigest() + "\n"
-
-                # write that line
-                fout.write(line)
-            # close the output file
+                    # write that line
+                    fout.write(line)
+                # close the output file
             fout.close()
         else:
             continue
 
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         pass
+print("Done! Show log.txt")
